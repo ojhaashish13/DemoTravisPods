084df3e08498c1e0fb7130e63ea8e72ae824b8e0
Ashish ojha <ojha.ashish13@gmail.com>
2014-12-04 12:00:09 +0530

    Updated script

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -24,20 +24,6 @@ zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dsym.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
-# if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
-#   echo ""
-#   echo "***************************"
-#   echo "* Uploading to Testflight *"
-#   echo "***************************"
-#   curl http://testflightapp.com/api/builds.json \
-#     -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
-#     -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-#     -F api_token="$TESTFLIGHT_API_TOKEN" \
-#     -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
-#     -F distribution_lists='Internal' \
-#     -F notes="$RELEASE_NOTES"
-# fi
-
 if [ ! -z "$HOCKEY_APP_ID" ] && [ ! -z "$HOCKEY_APP_TOKEN" ]; then
   echo ""
   echo "***************************"
